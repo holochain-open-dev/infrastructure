f45bff16271a95563d4e0130c12a89ef70d4b422
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-01-13 14:59:24 +0100

    Renamed ReadOnlyHoloHashMap

--- packages/stores/src/join-map.ts
+++ packages/stores/src/join-map.ts
@@ -1,4 +1,4 @@
-import { HoloHashMap, ReadableHoloHashMap } from "@holochain-open-dev/utils";
+import { HoloHashMap, ReadOnlyHoloHashMap } from "@holochain-open-dev/utils";
 import { HoloHash } from "@holochain/client";
 import { asyncDerived, join } from "./async-derived";
 import { AsyncReadable } from "./async-readable";
@@ -8,8 +8,8 @@ export function joinMap<
   H extends HoloHash,
   S,
   T extends AsyncReadable<S>,
-  M extends ReadableHoloHashMap<H, T>
->(holoHashMap: M): AsyncReadable<ReadableHoloHashMap<H, S>> {
+  M extends ReadOnlyHoloHashMap<H, T>
+>(holoHashMap: M): AsyncReadable<ReadOnlyHoloHashMap<H, S>> {
   const storeArray = holoHashMap
     .entries()
     .map(([key, store]) => asyncDerived([store], (v) => [key, v] as [H, S]));
