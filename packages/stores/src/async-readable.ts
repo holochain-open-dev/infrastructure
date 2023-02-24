ed55d1ee15fa1c1efce46b4bd0b639a3fe68bc68
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-24 14:49:33 +0100

    Fixed lazyLoadAndPoll

--- packages/stores/src/async-readable.ts
+++ packages/stores/src/async-readable.ts
@@ -1,3 +1,4 @@
+import { isEqual } from "lodash-es";
 import { readable, Readable, Subscriber, Unsubscriber } from "svelte/store";
 
 export type AsyncStatus<T> =
@@ -7,6 +8,27 @@ export type AsyncStatus<T> =
 
 export type AsyncReadable<T> = Readable<AsyncStatus<T>>;
 
+/**
+ * An `AsyncReadable<T>` is a `Readable` store that executes the given `Promise` the first time a subscriber subscribes to the store. It can have three states:
+ *
+ * - `pending`: the promise is still pending.
+ * - `error`: there was an error in the execution of the promise.
+ * - `complete`: the promise was completed.
+ *
+ * ```js
+ * import { asyncReadable } from '@holochain-open-dev/stores';
+ *
+ * const someResult = asyncReadable(async set => {
+ *   const value = await fetch("https://some/url");
+ *   set(value);
+ * });
+ *
+ * // Use as a normal svelte store
+ * someResult.subscribe(status => { console.log(status); }); // Will first print `{ status: 'pending' }`, and later print `{ status: 'complete', value: ... }`
+ * ```
+ *
+ * Like normal `readable` stores, it returns an unsubscribe function that gets called when the last subscriber unsubscribes:
+ */
 export function asyncReadable<T>(
   load: (set: Subscriber<T>) => Promise<Unsubscriber | void>
 ): AsyncReadable<T> {
@@ -23,6 +45,17 @@ export function asyncReadable<T>(
   });
 }
 
+/**
+ * Constructs an `AsyncReadable<T>` with the result of a promise, which it will execute only when the first subscriber subscribes.
+ *
+ * ```ts
+ * import { lazyLoad } from '@holochain-open-dev/stores';
+ *
+ * const someResult = lazyLoad(async () => fetch("https://some/url"));
+ *
+ * // Use someResult as any other `AsyncReadable<T>`
+ * ```
+ */
 export function lazyLoad<T>(load: () => Promise<T>): AsyncReadable<T> {
   return readable<AsyncStatus<T>>({ status: "pending" }, (set) => {
     load()
@@ -35,15 +68,34 @@ export function lazyLoad<T>(load: () => Promise<T>): AsyncReadable<T> {
   });
 }
 
+/**
+ * Constructs an `AsyncReadable<T>` that will call the given promise when the first subscriber subscribes,
+ * and then will start executing that same promise in the given polling interval until the last subscriber unsubscribes.
+ *
+ * The value of the store will be replaced with the polling result, only if the polling is successful and the new value is different from the old one.
+ * If the polling throws an error, that error is discarded.
+ *
+ * ```ts
+ * import { lazyLoadAndPoll } from '@holochain-open-dev/stores';
+ *
+ * const someResult = lazyLoadAndPoll(async () => fetch("https://some/url"), 1000); // Poll every one second
+ *
+ * // Use someResult as any other `AsyncReadable<T>`
+ * ```
+ */
 export function lazyLoadAndPoll<T>(
   load: () => Promise<T>,
   pollIntervalMs: number
 ): AsyncReadable<T> {
   return readable<AsyncStatus<T>>({ status: "pending" }, (set) => {
     let interval;
+    let currentValue;
     async function l() {
       const v = await load();
-      set({ status: "complete", value: v });
+      if (!isEqual(v, currentValue)) {
+        currentValue = v;
+        set({ status: "complete", value: v });
+      }
     }
     l()
       .then(() => {
