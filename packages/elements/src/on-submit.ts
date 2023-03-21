bdacd3b064e3cd4c39964af68dff13613f10d7b9
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-03-21 16:27:37 +0100

    Fix

--- packages/elements/src/on-submit.ts
+++ packages/elements/src/on-submit.ts
@@ -1,6 +1,35 @@
 import { AttributePart } from "lit";
 import { directive, Directive } from "lit/directive.js";
-import { serialize } from "@shoelace-style/shoelace/dist/utilities/form.js";
+
+function holoHashValue(value: any) {
+  if (typeof value === "string" && value.split(",").length === 39)
+    return new Uint8Array(value.split(",").map((s) => parseInt(s, 10)));
+  return value;
+}
+
+/**
+ * Serializes a form and returns a plain object. If a form control with the same name appears more than once, the
+ * property will be converted to an array.
+ */
+export function serialize(form: HTMLFormElement) {
+  const formData = new FormData(form);
+  const object: Record<string, unknown> = {};
+
+  formData.forEach((value, key) => {
+    if (Reflect.has(object, key)) {
+      const entry = object[key];
+      if (Array.isArray(entry)) {
+        entry.push(holoHashValue(value));
+      } else {
+        object[key] = [object[key], holoHashValue(value)];
+      }
+    } else {
+      object[key] = holoHashValue(value);
+    }
+  });
+
+  return object;
+}
 
 class OnSubmitDirective extends Directive {
   update(part: AttributePart, props: unknown[]) {
