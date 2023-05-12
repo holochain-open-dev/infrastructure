aedd1fcecee2ab7e97ef4724d990f4a58bc1a371
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-05-12 15:50:05 +0200

    Added more functions to pipe

--- packages/stores/src/pipe.ts
+++ packages/stores/src/pipe.ts
@@ -17,12 +17,39 @@ export function pipe<T, U, V, W>(
   fn2: (arg: U) => AsyncReadable<V>,
   fn3: (arg: V) => AsyncReadable<W>
 ): AsyncReadable<W>;
-export function pipe<T, U, V, W>(
+export function pipe<T, U, V, W, X>(
+  store: AsyncReadable<T>,
+  fn1: (arg: T) => AsyncReadable<U>,
+  fn2: (arg: U) => AsyncReadable<V>,
+  fn3: (arg: V) => AsyncReadable<W>,
+  fn4: (arg: W) => AsyncReadable<X>
+): AsyncReadable<X>;
+export function pipe<T, U, V, W, X, Y>(
+  store: AsyncReadable<T>,
+  fn1: (arg: T) => AsyncReadable<U>,
+  fn2: (arg: U) => AsyncReadable<V>,
+  fn3: (arg: V) => AsyncReadable<W>,
+  fn4: (arg: W) => AsyncReadable<X>,
+  fn5: (arg: X) => AsyncReadable<Y>
+): AsyncReadable<Y>;
+export function pipe<T, U, V, W, X, Y, Z>(
+  store: AsyncReadable<T>,
+  fn1: (arg: T) => AsyncReadable<U>,
+  fn2: (arg: U) => AsyncReadable<V>,
+  fn3: (arg: V) => AsyncReadable<W>,
+  fn4: (arg: W) => AsyncReadable<X>,
+  fn5: (arg: X) => AsyncReadable<Y>,
+  fn6: (arg: Y) => AsyncReadable<Z>
+): AsyncReadable<Z>;
+export function pipe<T, U, V, W, X, Y, Z>(
   store: AsyncReadable<T>,
   fn1: (arg: T) => AsyncReadable<U>,
   fn2?: (arg: U) => AsyncReadable<V>,
-  fn3?: (arg: V) => AsyncReadable<W>
-): AsyncReadable<W> {
+  fn3?: (arg: V) => AsyncReadable<W>,
+  fn4?: (arg: W) => AsyncReadable<X>,
+  fn5?: (arg: X) => AsyncReadable<Y>,
+  fn6?: (arg: Y) => AsyncReadable<Z>
+): AsyncReadable<Z> {
   let s: AsyncReadable<any> = asyncDeriveStore(store, fn1);
 
   if (fn2) {
@@ -30,6 +57,15 @@ export function pipe<T, U, V, W>(
   }
   if (fn3) {
     s = asyncDeriveStore(s, fn3);
+  }
+  if (fn4) {
+    s = asyncDeriveStore(s, fn4);
+  }
+  if (fn5) {
+    s = asyncDeriveStore(s, fn5);
+  }
+  if (fn6) {
+    s = asyncDeriveStore(s, fn6);
   }
 
   return s;
