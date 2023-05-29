74769b9066f3f14d748428ef6634dbbf933eb521
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-05-29 16:35:40 +0200

    Added svg helper function

--- packages/elements/src/icon.ts
+++ packages/elements/src/icon.ts
@@ -1,4 +1,9 @@
 // Wraps a path from @mdi/js into an svg, to be used inside an <sl-icon src=""></sl-icon>
 export function wrapPathInSvg(path: string): string {
-  return `data:image/svg+xml;utf8,<svg style='fill: currentColor' viewBox='0 0 24 24'><path d='${path}'></path></svg>`;
+  return `data:image/svg+xml;utf8,${wrapPathInSvgWithoutPrefix(path)}`;
+}
+
+// Wraps a path from @mdi/js into an svg, to be used inside an <sl-icon src=""></sl-icon>
+export function wrapPathInSvgWithoutPrefix(path: string): string {
+  return `<svg style='fill: currentColor' viewBox='0 0 24 24'><path d='${path}'></path></svg>`;
 }
