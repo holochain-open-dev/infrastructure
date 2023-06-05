4a42167a33d34beb67bed6ee9187313da86be628
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-06-05 17:14:20 +0200

    Fixed try count

--- packages/stores/src/retry-until-success.ts
+++ packages/stores/src/retry-until-success.ts
@@ -6,11 +6,15 @@ import { AsyncReadable, AsyncStatus } from "./async-readable.js";
 // Use this for queries you know are valid but may take some time to return a correct response (eg. eventual consistency)
 export function retryUntilSuccess<T>(
   fn: () => Promise<T>,
-  pollInterval: number = 1000
+  pollInterval: number = 1000,
+  maxRetries = 4
 ): AsyncReadable<T> {
   const store = writable<AsyncStatus<T>>({ status: "pending" });
 
+  let retriesCount = 0;
+
   const tryOnce = async () => {
+    retriesCount += 1;
     const value = await fn();
     store.set({
       status: "complete",
@@ -22,7 +26,14 @@ export function retryUntilSuccess<T>(
     try {
       await tryOnce();
     } catch (e) {
-      setTimeout(() => tryOnce(), pollInterval);
+      if (maxRetries > retriesCount) {
+        setTimeout(() => tryAndRetry(), pollInterval);
+      } else {
+        store.set({
+          status: "error",
+          error: e,
+        });
+      }
     }
   };
 
---
+++ packages/stores/tests/retry-until-success.test.js
@@ -0,0 +1,22 @@
+import { test, assert } from "vitest";
+import { get } from "svelte/store";
+import { retryUntilSuccess } from "../src";
+
+const sleep = (ms) => new Promise((r) => setTimeout(() => r(), ms));
+
+test("retryUntilSuccess", async () => {
+  let tryCount = 0;
+
+  const s = retryUntilSuccess(() => {
+    if (tryCount === 3) return true;
+    tryCount += 1;
+    throw new Error("My Error");
+  }, 10);
+
+  s.subscribe(() => {});
+
+  assert.equal(get(s).status, "pending");
+  await sleep(60);
+
+  assert.equal(get(s).status, "complete");
+});
