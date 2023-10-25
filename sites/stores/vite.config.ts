969fc7c0b2a941b7abcd9390b436e4ca4f4c98a0
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-10-25 18:14:42 +0200

    First exercise in store

--- sites/stores/vite.config.ts
+++ sites/stores/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import { vitePlugins } from "@mythosthesia/reveal-course-preset/vite-plugins.js";
 
 export default defineConfig({
+  optimizeDeps: {
+    disabled: true,
+  },
   base: "/common/stores",
   plugins: vitePlugins,
   build: {
