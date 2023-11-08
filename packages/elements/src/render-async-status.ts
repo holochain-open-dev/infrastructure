075445f711d812c894f123a90de1414f1a0f8c9a
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-11-08 13:44:37 +0100

    Added helpers

--- packages/elements/src/render-async-status.ts
+++ packages/elements/src/render-async-status.ts
@@ -8,42 +8,40 @@ export function withSpinnerAndDisplayError<T>(renderers: {
   completed: (value: T) => TemplateResult;
   error: ((error: any) => TemplateResult) | { label: string; tooltip: boolean };
 }) {
-  return (status: AsyncStatus<T>) =>
-    renderAsyncStatus(status, {
-      pending: () => html`<div
-        style="display: flex; flex-direction: column; align-items: center; justify-content: center; flex: 1;"
-      >
-        <sl-spinner style="font-size: 2rem;"></sl-spinner>
-      </div>`,
-      error: (e: any) =>
-        typeof renderers.error === "function"
-          ? renderers.error(e)
-          : html`<display-error
-              .headline=${renderers.error?.label}
-              .tooltip=${renderers.error?.tooltip}
-              .error=${e}
-            ></display-error> `,
-      completed: renderers.completed,
-    });
+  return renderAsyncStatus({
+    pending: () => html`<div
+      style="display: flex; flex-direction: column; align-items: center; justify-content: center; flex: 1;"
+    >
+      <sl-spinner style="font-size: 2rem;"></sl-spinner>
+    </div>`,
+    error: (e: any) =>
+      typeof renderers.error === "function"
+        ? renderers.error(e)
+        : html`<display-error
+            .headline=${renderers.error?.label}
+            .tooltip=${renderers.error?.tooltip}
+            .error=${e}
+          ></display-error> `,
+    completed: renderers.completed,
+  });
 }
 
 /**
  * Renders the given AsyncStatus with the given renderers
  */
-export function renderAsyncStatus<T>(
-  status: AsyncStatus<T>,
-  renderers: {
-    completed: (value: T) => TemplateResult;
-    error: (error: any) => TemplateResult;
-    pending: () => TemplateResult;
-  }
-): TemplateResult {
-  switch (status.status) {
-    case "complete":
-      return renderers.completed(status.value);
-    case "error":
-      return renderers.error(status.error);
-    default:
-      return renderers.pending();
-  }
+export function renderAsyncStatus<T>(renderers: {
+  completed: (value: T) => TemplateResult;
+  error: (error: any) => TemplateResult;
+  pending: () => TemplateResult;
+}) {
+  return (status: AsyncStatus<T>) => {
+    switch (status.status) {
+      case "complete":
+        return renderers.completed(status.value);
+      case "error":
+        return renderers.error(status.error);
+      default:
+        return renderers.pending();
+    }
+  };
 }
