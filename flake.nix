c22522b49bb582d652c012ca5bf5c04d29abec57
guillem.cordoba <guillem.cordoba@gmail.com>
2024-03-04 17:03:54 +0100

    Refix arg name

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       {
 			  flake = {
 					lib = {
-						rustZome = { cratePath, holochain, workspacePath, optimizeWasms ? true, excludedCrates ? [] }: 
+						rustZome = { cratePath, holochain, workspacePath, optimizeWasm ? true, excludedCrates ? [] }: 
 							let 
 							  system = holochain.devShells.holonix.system;
 							  pkgs = import inputs.nixpkgs {
@@ -44,7 +44,7 @@
 
 							in
 								pkgs.callPackage ./nix/zome.nix {
-					        inherit craneLib cratePath excludedCrates workspacePath optimizeWasms;
+					        inherit craneLib cratePath excludedCrates workspacePath optimizeWasm;
 					      };
 						dna = { holochain, dnaManifest, zomes }: 
 							let 
