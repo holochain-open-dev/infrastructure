ccba68beb7bdbcec6aeae9c62cd8096d331abe65
guillem.cordoba <guillem.cordoba@gmail.com>
2024-03-05 15:14:43 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -33,6 +33,10 @@
 						filterDnas = filterByHolochainPackageType "dna";
 						filterHapps = filterByHolochainPackageType "happ";
 
+						filterZomesFromSources = sources: builtins.foldl' (acc: source: acc // filterZomes source) {} sources;
+						filterDnasFromSources = sources: builtins.foldl' (acc: source: acc // filterDnas source) {} sources;
+						filterHappsFromSources = sources: builtins.foldl' (acc: source: acc // filterHapps source) {} sources;
+
 						rustZome = { cratePath, holochain, workspacePath, excludedCrates ? [] }: 
 							let 
 							  system = holochain.devShells.holonix.system;
