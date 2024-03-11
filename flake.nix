8ae6c8817918006f25f0e38c0351809e23328df8
guillem.cordoba <guillem.cordoba@gmail.com>
2024-03-11 16:22:06 +0100

    Deterministic wasm on linux x86_64

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
 								    overlays = [ (import inputs.rust-overlay) ];
 								  };
 
-								  rustToolchain = pkgs.rust-bin.nightly."2024-01-29".minimal.override {
+								  rustToolchain = pkgs.rust-bin.stable."1.75.0".minimal.override {
 								    # Set the build targets supported by the toolchain,
 								    # wasm32-unknown-unknown is required for trunk.
 								    targets = [ "wasm32-unknown-unknown" ];
@@ -65,7 +65,7 @@
 							    overlays = [ (import inputs.rust-overlay) ];
 							  };
 
-							  rustToolchain = pkgs.rust-bin.nightly."2024-01-29".minimal.override {
+							  rustToolchain = pkgs.rust-bin.stable."1.75.0".minimal.override {
 							    # Set the build targets supported by the toolchain,
 							    # wasm32-unknown-unknown is required for trunk.
 							    targets = [ "wasm32-unknown-unknown" ];
--- nix/zome.nix
+++ nix/zome.nix
@@ -1,5 +1,4 @@
 { 
-	pkgs,
 	runCommandLocal,
 	runCommandNoCC,
 	binaryen,
@@ -11,7 +10,7 @@
 }:
 
 let 
-	cargoExtraArgs = "-Z avoid-dev-deps --workspace ${if excludedCrates != null then builtins.concatStringsSep " " (builtins.map (excludedCrate: ''--exclude ${excludedCrate}'') excludedCrates) else ''''}";
+	cargoExtraArgs = "--workspace ${if excludedCrates != null then builtins.concatStringsSep " " (builtins.map (excludedCrate: ''--exclude ${excludedCrate}'') excludedCrates) else ''''}";
 
 	cargoToml = builtins.fromTOML (builtins.readFile crateCargoToml);
   crate = cargoToml.package.name;
@@ -52,43 +51,14 @@ let
 			version = "workspace";
 		});
 
-  #   wasm = deterministicCraneLib.buildPackage (commonArgs // {
-		# 	cargoToml = crateCargoToml;
-		# 	cargoLock = workspacePath + /Cargo.lock;
-		# 	cargoArtifacts = wasmDeps;
-		# 	cargoBuildCommand = "mkdir /build/vendor && cd $cargoVendorDir   && echo '${builtins.readFile ./vendored-sources-config.toml}' > /build/source/.cargo-home/config.toml && cat /build/source/.cargo-home/config.toml && cargo build --profile release -v";
-		# 	cargoExtraArgs = "-p ${crate} --locked";
-		#   pname = crate;
-		# 	version = cargoToml.package.version;
-		# });
-	  rustToolchain = pkgs.rust-bin.stable.latest.minimal.override {
-	    # Set the build targets supported by the toolchain,
-	    # wasm32-unknown-unknown is required for trunk.
-	    targets = [ "wasm32-unknown-unknown" ];
-	  };
-		rustPlatform = pkgs.makeRustPlatform {
-	    cargo = rustToolchain;
-	    rustc = rustToolchain;
-		};
-
-		wasm = rustPlatform.buildRustPackage {
-			nativeBuildInputs = [
-				pkgs.llvmPackages.bintools
-			];
-	    src = workspacePath;
-			pname = crate;
+    wasm = deterministicCraneLib.buildPackage (commonArgs // {
+			cargoToml = crateCargoToml;
+			cargoLock = workspacePath + /Cargo.lock;
+			cargoArtifacts = wasmDeps;
+			cargoExtraArgs = "-p ${crate} --locked";
+		  pname = crate;
 			version = cargoToml.package.version;
-			cargoLock = {
-				lockFile = "${workspacePath}/Cargo.lock";
-			};
-			cargoBuildFlags = "--target wasm32-unknown-unknown";
-			doCheck = false;
-			CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
-	    installPhase = ''
-	      mkdir -p $out/lib
-	      cp target/wasm32-unknown-unknown/release/*.wasm $out/lib/
-	    '';
-		};
+		});
 	in
 		runCommandLocal "${crate}-deterministic" {
 			meta = {
