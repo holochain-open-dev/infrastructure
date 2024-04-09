4c2cbc358821cdd9825feb183a04b7b961913514
guillem.cordoba <guillem.cordoba@gmail.com>
2024-04-09 14:14:14 +0200

    Fixed nix

--- flake.nix
+++ flake.nix
@@ -170,7 +170,7 @@ If you are trying to run a script for a certain workspace like \`npm run build -
 								commonArgs = {
 									strictDeps = true;
 									doCheck = false;
-								  src = craneLib.cleanCargoSource (craneLib.path cratePath);
+								  src = craneLib.cleanCargoSource (craneLib.path ./.);
 								};
 							in 
 								craneLib.buildPackage (commonArgs // {
@@ -187,10 +187,23 @@ If you are trying to run a script for a certain workspace like \`npm run build -
 								cargoToml = builtins.fromTOML (builtins.readFile "${cratePath}/Cargo.toml");
 							  crate = cargoToml.package.name;
 
+								buildInputs = (with pkgs; [ openssl inputs'.holochain.packages.opensslStatic sqlcipher ])
+								  ++ (lib.optionals pkgs.stdenv.isDarwin
+								  (with pkgs.darwin.apple_sdk_11_0.frameworks; [
+								    AppKit
+								    CoreFoundation
+								    CoreServices
+								    Security
+								    IOKit
+								  ]));
 								commonArgs = {
+								  inherit buildInputs;
 									strictDeps = true;
 									doCheck = false;
-								  src = craneLib.cleanCargoSource (craneLib.path cratePath);
+								  src = craneLib.cleanCargoSource (craneLib.path ./.);
+							    nativeBuildInputs = (with pkgs; [ makeWrapper perl pkg-config inputs'.holochain.packages.goWrapper ])
+							      ++ lib.optionals pkgs.stdenv.isDarwin
+							      (with pkgs; [ xcbuild libiconv ]);
 								};
 							in 
 								craneLib.buildPackage (commonArgs // {
