7c6e75239cc6094f07f5bb328d0e05f908ee27b0
guillem.cordoba <guillem.cordoba@gmail.com>
2024-04-11 11:06:44 +0200

    Added tauri deps to sweettest

--- nix/sweettest.nix
+++ nix/sweettest.nix
@@ -13,6 +13,25 @@ let
   crate = cargoToml.package.name;
 	
   buildInputs = (with pkgs; [ openssl holochain.packages.opensslStatic sqlcipher glib ])
+    ++ (lib.optionals pkgs.stdenv.isLinux
+    (with pkgs; [
+      webkitgtk_4_1.dev
+      gdk-pixbuf
+      gtk3
+      # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+      gst_all_1.gstreamer
+      # Common plugins like "filesrc" to combine within e.g. gst-launch
+      gst_all_1.gst-plugins-base
+      # Specialized plugins separated by quality
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      # Plugins to reuse ffmpeg to play almost every video format
+      gst_all_1.gst-libav
+      # Support the Video Audio (Hardware) Acceleration API
+      gst_all_1.gst-vaapi
+      libsoup_3
+    ]))
     ++ (lib.optionals pkgs.stdenv.isDarwin
     (with pkgs.darwin.apple_sdk_11_0.frameworks; [
       AppKit
