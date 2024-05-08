a9829f9350883587d0d767a760a80d336ae4e792
guillem.cordoba <guillem.cordoba@gmail.com>
2024-05-08 13:48:58 +0200

    Added pipe

--- packages/signals/src/index.ts
+++ packages/signals/src/index.ts
@@ -1,19 +1,20 @@
-export * from "signal-polyfill";
+export * from 'signal-polyfill';
 // Omit joinAsyncMap because we provide the holohash implementation version in join-map.ts
 export {
-  AsyncComputed,
-  AsyncResult,
-  AsyncSignal,
-  AsyncState,
-  fromPromise,
-  joinAsync,
-  JoinAsyncOptions,
-  toPromise,
-} from "async-signals";
-export * from "lit-signal-watcher";
+	AsyncComputed,
+	AsyncResult,
+	AsyncSignal,
+	AsyncState,
+	fromPromise,
+	joinAsync,
+	JoinAsyncOptions,
+	toPromise,
+	pipe,
+} from 'async-signals';
+export * from 'lit-signal-watcher';
 
-export * from "./join-map.js";
-export * from "./utils.js";
-export * from "./holochain.js";
-export * from "./watch.js";
-export * from "./map-completed.js";
+export * from './join-map.js';
+export * from './utils.js';
+export * from './holochain.js';
+export * from './watch.js';
+export * from './map-completed.js';
