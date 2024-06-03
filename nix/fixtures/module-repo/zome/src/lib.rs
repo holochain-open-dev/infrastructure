9535a07f878ea0812ece96d08d216ca29c82f538
guillem.cordoba <guillem.cordoba@gmail.com>
2024-06-03 17:39:23 +0200

    Fix test

--- nix/fixtures/module-repo/zome/src/lib.rs
+++ nix/fixtures/module-repo/zome/src/lib.rs
@@ -2,6 +2,5 @@ use hdk::prelude::*;
 
 #[hdk_extern]
 pub fn hello(_: ()) -> ExternResult<String> {
-    let e = wasm_error!(WasmErrorInner::Guest(String::from("hi")));
-    Ok(format!("hello {e:?}"))
+    Ok(String::from("hi"))
 }
