1f8677520f24aab0bda2df1858c75969ddca7c5b
guillem.cordoba <guillem.cordoba@gmail.com>
2024-10-10 11:22:02 +0200

    hashAction

--- packages/utils/src/hash.ts
+++ packages/utils/src/hash.ts
@@ -1,4 +1,7 @@
 import {
+	Action,
+	ActionHash,
+	ActionType,
 	Entry,
 	EntryHash,
 	HoloHash,
@@ -63,10 +66,141 @@ export function isHash(hash: string): boolean {
 	].find(prefix => hash.startsWith(`u${prefix}`));
 }
 
+// The hash of an action depends on the order of keys
+// To make it deterministic and aligned with hashes of actions coming from holochain
+// we need to enforce the order of actions to align with the holochain ones
+function sortActionKeys(action: Action): Action {
+	const weight = (action as any).weight;
+	switch (action.type) {
+		case ActionType.Dna:
+			return {
+				type: ActionType.Dna,
+				author: action.author,
+				timestamp: action.timestamp,
+				hash: action.hash,
+			};
+		case ActionType.AgentValidationPkg:
+			return {
+				type: ActionType.AgentValidationPkg,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+				membrane_proof: action.membrane_proof,
+			};
+		case ActionType.InitZomesComplete:
+			return {
+				type: ActionType.InitZomesComplete,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+			};
+		case ActionType.CreateLink:
+			return {
+				type: ActionType.CreateLink,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+
+				base_address: action.base_address,
+				target_address: action.target_address,
+				zome_index: action.zome_index,
+				link_type: action.link_type,
+				tag: action.tag,
+				weight: {
+					bucket_id: action.weight.bucket_id,
+					units: action.weight.units,
+				},
+			};
+		case ActionType.DeleteLink:
+			return {
+				type: ActionType.DeleteLink,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+				base_address: action.base_address,
+				link_add_address: action.link_add_address,
+			};
+		case ActionType.OpenChain:
+			return {
+				type: ActionType.OpenChain,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+				prev_dna_hash: action.prev_dna_hash,
+			};
+		case ActionType.CloseChain:
+			return {
+				type: ActionType.CloseChain,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+				new_dna_hash: action.new_dna_hash,
+			};
+		case ActionType.Create:
+			return {
+				type: ActionType.Create,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+				entry_type: action.entry_type,
+				entry_hash: action.entry_hash,
+				weight: {
+					bucket_id: weight.bucket_id,
+					units: weight.units,
+					rate_bytes: weight.rate_bytes,
+				},
+			} as any;
+		case ActionType.Update:
+			return {
+				type: ActionType.Update,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+
+				original_action_address: action.original_action_address,
+				original_entry_address: action.original_entry_address,
+
+				entry_type: action.entry_type,
+				entry_hash: action.entry_hash,
+				weight: {
+					bucket_id: weight.bucket_id,
+					units: weight.units,
+					rate_bytes: weight.rate_bytes,
+				},
+			} as any;
+		case ActionType.Delete:
+			return {
+				type: ActionType.Delete,
+				author: action.author,
+				timestamp: action.timestamp,
+				action_seq: action.action_seq,
+				prev_action: action.prev_action,
+
+				deletes_address: action.deletes_address,
+				deletes_entry_address: action.deletes_address,
+				weight: {
+					bucket_id: weight.bucket_id,
+					units: weight.units,
+				},
+			} as any;
+	}
+}
+
+export function hashAction(action: Action): ActionHash {
+	return hash(sortActionKeys(action), HashType.ACTION);
+}
+
 // From https://github.com/holochain/holochain/blob/dc0cb61d0603fa410ac5f024ed6ccfdfc29715b3/crates/holo_hash/src/encode.rs
 export function hash(content: any, type: HashType): HoloHash {
-	const obj = isPlainObject(content) ? sortKeys(content) : content;
-	const bytesHash: Uint8Array = blake.blake2b(encode(obj), null, 32);
+	const bytesHash: Uint8Array = blake.blake2b(encode(content), null, 32);
 
 	const fullhash = new Uint8Array([
 		...Base64.toUint8Array(getPrefix(type)),
--- packages/utils/tests/hash.test.ts
+++ packages/utils/tests/hash.test.ts
@@ -1,18 +1,48 @@
-import { encodeHashToBase64 } from "@holochain/client";
-import { test, assert } from "vitest";
-import { hash, HashType } from "../src/index.js";
+import {
+	ActionType,
+	decodeHashFromBase64,
+	encodeHashToBase64,
+	fakeAgentPubKey,
+	fakeDnaHash,
+} from '@holochain/client';
+import { assert, test } from 'vitest';
 
-test("test hash", async () => {
-  const h = hash("asdfsadf", HashType.ACTION);
-  assert.equal(
-    encodeHashToBase64(h),
-    "uhCkkboYJIWR7sh8l4i7OTWdOm3iY9ikij1-D9JLoqar86YF4KNht"
-  );
+import {
+	HashType,
+	fakeCreateAction,
+	fakeDeleteLinkAction,
+	hash,
+	hashAction,
+} from '../src/index.js';
+
+test('test hash', async () => {
+	const h = hash('asdfsadf', HashType.ENTRY);
+	assert.equal(
+		encodeHashToBase64(h),
+		'uhCEkboYJIWR7sh8l4i7OTWdOm3iY9ikij1-D9JLoqar86YF4KNht',
+	);
 });
 
-test("test ordering", async () => {
-  assert.deepEqual(
-    hash({ first: 1, second: 2 }, HashType.ACTION),
-    hash({ second: 2, first: 1 }, HashType.ACTION)
-  );
+test('test action hash', async () => {
+	const h = hashAction({
+		type: ActionType.Dna,
+		timestamp: 1728551987340000,
+		hash: decodeHashFromBase64(
+			'uhC0kxfhaMcQ5m73u42s3tiWXWOeS2mTy7Ovj8oHn0Bilvmndtzun',
+		),
+		author: decodeHashFromBase64(
+			'uhCAky816aQkHDHniTt-F0ccT0bAZFz6wUAq5jL7H1q8Ii8h62MG8',
+		),
+	});
+	assert.equal(
+		encodeHashToBase64(h),
+		'uhCkkABYPHZNDn-U4AVhuev41b54I4xCPuLMOD-eLxy8lRNT9Fd7f',
+	);
 });
+
+// test("test ordering", async () => {
+//   assert.deepEqual(
+//     hash({ first: 1, second: 2 }, HashType.ACTION),
+//     hash({ second: 2, first: 1 }, HashType.ACTION)
+//   );
+// });
