3b6fefae3a80295c5b0d55e1e8f4627382f4c4c7
Matthias <tuttiacc@hotmail.com>
2025-01-30 12:09:51 +0100

    fix svg and make URI safe

--- packages/elements/src/icon.ts
+++ packages/elements/src/icon.ts
@@ -5,5 +5,5 @@ export function wrapPathInSvg(path: string): string {
 
 // Wraps a path from @mdi/js into an svg, to be used inside an <sl-icon src=""></sl-icon>
 export function wrapPathInSvgWithoutPrefix(path: string): string {
-  return `<svg style='fill: currentColor' viewBox='0 0 24 24'><path d='${path}'></path></svg>`;
+  return encodeURIComponent(`<svg xmlns='http://www.w3.org/2000/svg' style='fill: currentColor' viewBox='0 0 24 24'><path d='${path}'></path></svg>`);
 }
