bf2ac15791e02020c77023ef6696375ecc6e1403
ddd-mtl <doute.d@gmail.com>
2025-12-05 14:53:41 -0500

    Removed core-types definition that have been added in holochain-client-js 0.20.1

--- packages/core-types/src/common.ts
+++
@@ -1,6 +0,0 @@
-import { HoloHash, HoloHashB64 } from "@holochain/client";
-
-export type DhtOpHashB64 = HoloHashB64;
-
-export type DhtOpHash = HoloHash;
-
--- packages/core-types/src/index.ts
+++ packages/core-types/src/index.ts
@@ -1,5 +1,2 @@
-export * from "./common.js";
 export * from "./entry.js";
-export * from "./entry-details.js";
 export * from "./dna-file.js";
-export * from "./validation.js";
